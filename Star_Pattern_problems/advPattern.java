df5eb0a254a3f535a0b03eea69371c5d1afcb9c2
Ujjwal-kumar-12 <ukumar101201@gmail.com>
2026-02-10 19:25:59 +0530

    .

--- Star_Pattern_problems/advPattern.java
+++ Star_Pattern_problems/advPattern.java
@@ -4,12 +4,24 @@ public class advPattern {
     public static void main(String[] args) {
         char ch='*';
         int n=4;
-        int m=5;
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
+            for(int j=1;j<=n-i;j++){
                 System.out.print(" ");
             }
+            for(int k=1;k>=2*n-i;k++){
+                System.out.print(ch);
+            }
+            System.out.println();
+        }
+        for(int m=n;m>=1;m--)
+        {
+        for(int i=1;i<=5-m;i++){
+                System.out.print(" ");
+            }
+            for (int l=1;l<=2*n-1;l++) {
+            System.out.print(ch);
+            }
+        System.out.println();
         }
     }
-    
 }
